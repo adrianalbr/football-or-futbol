c58f2fe8454c73cb430fa599bc26abd3dc6905d8
Arpita Saha <arpita.sahaa@gmail.com>
2020-12-14 19:33:28 -0500

    recovered user model

--- models/user.js
+++ models/user.js
@@ -2,12 +2,20 @@ module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User", {
       firstName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate : {
+          min: 2
+        }
       },
       lastName: {
         type: DataTypes.STRING,
+        allowNull: false,
+        
       },
       email: {
         type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
         validate: {
             isEmail: true
           }
