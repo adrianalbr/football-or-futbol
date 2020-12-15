6dbdf93fec93c3b5637e1ced11ccc0296f952426
Arpita Saha <arpita.sahaa@gmail.com>
2020-12-14 19:30:50 -0500

    Removed headToHead.js

--- models/headToHead.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    const Head = sequelize.define("Head", {
-      firstName: {
-        type: DataTypes.STRING,
-      },
-    });
-    return Head;
-  };
-  
