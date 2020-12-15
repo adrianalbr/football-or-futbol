eeab0b3071e80e6fd835de08238e9f7d9c9cbaf3
Arpita Saha <arpita.sahaa@gmail.com>
2020-12-14 20:55:25 -0500

    added validations

--- models/players.js
+++ models/players.js
@@ -16,32 +16,72 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate : {
-        isNumeric: true, 
+        isNumeric: true,
+        len: [1,99] 
       }
     },
     speed: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     strength: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     agility: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     kickPower: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     tackle: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     jumping: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     stamina: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate : {
+        isNumeric: true,
+        len: [1,99] 
+      }
     },
     source: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate : {
+        isAlpha: true
+      }
     },
   });
 
