c76fc4f2ab343e640c69036cda16a19ceb0ef6b9
Dani JM <xdanielmtz@gmail.com>
2020-12-16 14:00:44 -0500

    added routes for login and index in html routes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
   app.get("/", (req, res) => {
-    res.render("index");
-  });
-  app.get("/login", function (req, res) {
     res.render("login");
+  });
+  app.get("/index", function (req, res) {
+    res.render("index");
   });
   app.get("/headTohead", function (req, res) {
     res.render("headTohead");
