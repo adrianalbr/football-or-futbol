0e575e609a7f17e2017bddca61934177ff0c67fb
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2020-12-16 19:01:21 -0700

    accepting current changes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,14 +1,5 @@
 module.exports = function (app) {
   app.get("/", (req, res) => {
-<<<<<<< HEAD
-=======
-    res.render("login");
-  });
-  app.get("/index", (req, res) => {
-    res.render("index");
-  });
-  app.get("/login", function (req, res) {
->>>>>>> f2d04a92222d61d0868716acf6b2bca6ba9b18d2
     res.render("login");
   });
   app.get("/index", function (req, res) {
