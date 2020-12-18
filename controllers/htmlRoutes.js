9b8c5ab67b629d28e3488e2ddb8ae756cf54e71d
Arpita Saha <arpita.sahaa@gmail.com>
2020-12-18 12:26:11 -0500

    linked login in and signup pages

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,15 +1,10 @@
 module.exports = function (app) {
   app.get("/", (req, res) => {
-    res.render("login");
-  });
-  app.get("/futbol", (req, res) => {
-    res.render("futbol");
-  });
-  app.get("/football", (req, res) => {
-    res.render("football");
-  });
-  app.get("/signup", function (req, res) {
     res.render("signup");
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("login");
   });
   app.get("/index", function (req, res) {
     res.render("index");
